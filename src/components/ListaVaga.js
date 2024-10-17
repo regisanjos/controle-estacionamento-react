88dfda4b99b52c7bbf58a62474cd0aa8f2fcda98
regisanjos <regisanjos47@gmail.com>
2024-10-17 05:51:45 -0300

    acrecentando botao de remover e reftorando algumas coisas como digitaçao errada

--- src/components/ListaVaga.js
+++ src/components/ListaVaga.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
 
 const ListaVaga = () => {
-  const vagas = [
+const [vagas, setVagas] = useState ([
+  
     { placa: 'AAA-1234', nome: 'Geromel', apartamento: '101', bloco: 'A', vaga: '12A' },
     { placa: 'BBB-5678', nome: 'Felipe', apartamento: '202', bloco: 'B', vaga: '15B' }
-  ];
+  ]);
 
+  const removerVaga = (index) =>{
+    const novasVagas = vagas.filter((_,i) => i !== index );
+    setVagas(novasVagas);
+  }
   return (
     <div>
       <h3>Vagas Cadastradas</h3>
@@ -15,6 +21,9 @@ const ListaVaga = () => {
             <span>{vaga.nome} - {vaga.placa}</span>
             <span>Apt: {vaga.apartamento} - Bloco: {vaga.bloco}</span>
             <span>Vaga: {vaga.vaga}</span>
+            <button onClick={() => removerVaga(index)} className='button-removr'>
+              Remover
+            </button>
           </li>
         ))}
       </ul>
